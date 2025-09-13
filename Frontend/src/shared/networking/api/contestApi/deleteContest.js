ba3e7a85db77c49f0fa9cd8e96a6f60442ff74c4
Gaurav <sahnigaurav17@gmail.com>
2025-09-13 15:34:40 +0530

    Make api for updating and deleting contest

---
+++ Frontend/src/shared/networking/api/contestApi/deleteContest.js
@@ -0,0 +1,26 @@
+const deleteContest = async (id) => {
+    try {
+      const token=localStorage.getItem('token');
+      const response = await fetch(`${import.meta.env.VITE_SERVER_URL}/api/contests/${id}`, {
+        method: 'DELETE',
+        headers: {
+          'Authorization': `Bearer ${token}`,
+          'Content-Type': 'application/json'
+      },
+      });
+  
+      const data = await response.json();
+  
+      if (!response.ok) {
+        return { error: data.error || 'Failed to delete question' };
+      }
+  
+      return data;
+    } catch (error) {
+      console.error('Error:', error);
+      return { error: 'Something went wrong.' };
+    }
+  };
+  
+  export { deleteContest };
+  
--- Frontend/src/shared/networking/api/contestApi/updateContest.js
+++ Frontend/src/shared/networking/api/contestApi/updateContest.js
@@ -1,7 +1,7 @@
-const createContest=async(contestData)=> {
+const updateContest=async(contestData)=> {
     try {
         const token=localStorage.getItem('token');
-        const response = await fetch(`${import.meta.env.VITE_SERVER_URL}/api/contests/create`, {
+        const response = await fetch(`${import.meta.env.VITE_SERVER_URL}/api/contests/update`, {
             method: 'POST',
             headers: {
                 'Authorization': `Bearer ${token}`,
@@ -11,13 +11,13 @@ const createContest=async(contestData)=> {
         });
         const result = await response.json();
         if (!response.ok) {
-            return { error: result.error || 'Failed to create contest' };
+            return { error: result.error || 'Failed to update Contest' };
         }
         return result;
     } catch (error) {
-        console.error('Error creating contest:', error.message);
+        console.error('Error updating contest:', error.message);
         throw error;
     }
 }
 
-export { createContest };
+export { updateContest };
